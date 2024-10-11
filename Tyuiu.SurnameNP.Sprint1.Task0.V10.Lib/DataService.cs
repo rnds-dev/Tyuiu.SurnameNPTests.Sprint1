d8feb6daa7c48d5d6b40cccc71b256d0c5299382
rnds-dev <itsnotslava@gmail.com>
2024-10-11 16:27:29 +0500

    Add project files.

---
+++ Tyuiu.SurnameNP.Sprint1.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SurnameNP.Sprint1.Task0.V10.Lib
+{
+	public class DataService : ISprint1Task0V10
+	{
+		public double Calculate()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
