fabc6711bf1f052cad98f435303f01ae21b68de5
rnds-dev <itsnotslava@gmail.com>
2024-10-11 17:03:11 +0500

    - task 0

--- Tyuiu.SurnameNPTests.Sprint1/Program.cs
+++ Tyuiu.SurnameNPTests.Sprint1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.SurnameNPTests.Sprint1.Task2.V10.Lib;
+
+namespace Tyuiu.SurnameNPTests.Sprint1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DataService ds = new DataService();
+
+			Console.WriteLine(ds.ConvertMetreToInchs(0));
+			Console.ReadKey();
+		}
+	}
+}
