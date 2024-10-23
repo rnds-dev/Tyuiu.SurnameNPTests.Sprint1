b038eac5d888cabcc58eb85e93f1f2a013fd1d6c
rnds-dev <itsnotslava@gmail.com>
2024-10-23 13:23:52 +0500

    + app & tests

---
+++ Tyuiu.SurnameNPTests.Sprint1.Task0.V10.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SurnameNPTests.Sprint1.Task0.V10.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ Tyuiu.SurnameNPTests.Sprint1.Task3.V10.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SurnameNPTests.Sprint1.Task3.V10.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
