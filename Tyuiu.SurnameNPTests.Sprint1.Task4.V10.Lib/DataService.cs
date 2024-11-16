60ab8e7f66bb8b9be2a20e3f7d4758a16caba446
rnds-dev <itsnotslava@gmail.com>
2024-11-16 22:47:55 +0500

    + stack overflow task

---
+++ Tyuiu.SurnameNPTests.Sprint1.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿// StackOverflow
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SurnameNPTests.Sprint1.Task4.V10
+{
+	public class DataService : ISprint1Task4V10
+	{
+		public double Calculate(double a)
+		{
+			return Calculate(a + 1);
+		}
+	}
+}
---
+++ Tyuiu.SurnameNPTests.Sprint1.Task4.V10.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SurnameNPTests.Sprint1.Task4.V10.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
